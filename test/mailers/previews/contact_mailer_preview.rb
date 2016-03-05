7edcc58d8da813aa1441482f8b6a38f5c6865d06
dorianwolf <dorian.wolf@gmail.com>
2016-03-04 19:07:20 -0800

    mailer complete, I think, assuming figaro implemented correctly

---
+++ test/mailers/previews/contact_mailer_preview.rb
@@ -0,0 +1,6 @@
+# Preview all emails at http://localhost:3000/rails/mailers/contact_mailer
+class ContactMailerPreview < ActionMailer::Preview
+  def new_contact_email_preview
+    ContactMailer.new_contact_email(Contact.first)
+  end
+end
